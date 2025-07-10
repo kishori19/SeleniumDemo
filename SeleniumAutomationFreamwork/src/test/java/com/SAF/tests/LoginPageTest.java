fe4645f1676801a1c4e9535f79f03e067f146736
kishori19 <kishorishinge1910@gmail.com>
2025-07-10 20:21:06 +0530

    Initial commit

---
+++ SeleniumAutomationFreamwork/src/test/java/com/SAF/tests/LoginPageTest.java
@@ -0,0 +1,12 @@
+package com.SAF.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+
+    @Test
+    public void login()
+    {
+        System.out.println("Hello This is my first Code");
+    }
+}
